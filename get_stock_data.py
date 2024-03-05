c98fc89498f4d1fae3cf0c9731f1ca411e5c3b91
Zohaib <zohaibshahid520@gmail.com>
2024-03-05 05:50:21 -0700

    Update get_stock_data.py

--- get_stock_data.py
+++ get_stock_data.py
@@ -24,25 +24,31 @@ def get_daily_stock_data(tickerSymbol,start_date,end_date):
         selected_tickerDf = tickerDf.loc[:, config.SELECTED_COLUMNS]
 
         # Extracting the date from the "Date" column
-        selected_tickerDf["Date"] = pd.to_datetime(selected_tickerDf["Date"], utc=True).dt.date
+        selected_tickerDf["Date"] = pd.to_datetime(selected_tickerDf.index, utc=True).date
 
         # Adding the Ticker Column
         selected_tickerDf["Ticker"] = tickerSymbol
+
+        # Removing the date index
+        stocks_data = selected_tickerDf.reset_index(drop=True)
 
         logging.info(
             "Extraction process completed for ticker: %s",
             tickerSymbol,
         )
     
+        return stocks_data
+    
     except Exception as e:
         logging.error("An error occurred in the extract function: %s", str(e))
 
-    return selected_tickerDf
 
 if __name__ == "__main__":
     logging.info("Application started.")
     end_date = date.today()
+    print("Current Date:", end_date)
     start_date = end_date - timedelta(days=5 * 365)
+    print("Date 5 Years Ago:", start_date)
     stocks_data = get_daily_stock_data("DIS",start_date,end_date)
     print(stocks_data)
     logging.info("Application finished.")
