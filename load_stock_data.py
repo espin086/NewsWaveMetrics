4caddc5df8abca2f9db25a610250d66de136a098
zohaibshahid520@gmail.com <zohaibshahid520@gmail.com>
2024-03-06 13:44:13 +0300

    Update load_stock_data.py

---
+++ load_stock_data.py
@@ -0,0 +1,14 @@
+import config
+import logging
+from SQLiteHandler import create_stock_table, upload_stock_to_db
+
+def load(stock_data):
+    """
+    This function loads the dataframe to the database.
+    """
+    logging.info("Loading Function Initiated")
+    # Creating the db and table 
+    create_stock_table()
+    # Uploading the data to db
+    upload_stock_to_db(df = stock_data)
+
