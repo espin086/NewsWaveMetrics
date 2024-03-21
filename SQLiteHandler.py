1f399c73573c6166965d3e0bd872224ddb8883f5
Zohaib <zohaibshahid520@gmail.com>
2024-03-21 04:12:00 -0600

    Updated SQLiteHandler.py

--- SQLiteHandler.py
+++ SQLiteHandler.py
@@ -65,6 +65,38 @@ def create_news_table():
     finally:
         conn.close()
 
+def create_fred_table():
+    """Create the database if it doesn't exist."""
+    logging.info("Checking and creating database if not present.")
+    conn = sqlite3.connect(config.DATABASE)
+    c = conn.cursor()
+
+    try:
+        c.execute(
+            f"""CREATE TABLE IF NOT EXISTS {config.TABLE_FRED_DATA}
+                    (date TEXT,
+                    exchange_rates REAL,
+                    treasury_yields REAL,
+                    fed_funds_rate REAL,
+                    consumer_price_index REAL,
+                    GDP  REAL,
+                    industrial_production REAL,
+                    unemployment_rate REAL,
+                    consumer_sentiment REAL,
+                    producer_price_index REAL,
+                    PRIMARY KEY (date)
+                    )"""
+        )
+        conn.commit()
+        logging.info(
+            "Successfully created or ensured the table %s exists.",
+            config.TABLE_FRED_DATA,
+        )
+    except Exception as e:
+        logging.error("Failed to create table: %s", e)
+    finally:
+        conn.close()
+
 def upload_stock_to_db(df):
     """Check if the primary key exists in the database and upload data if not."""
     logging.info("Starting upload to database.")
@@ -171,5 +203,52 @@ def upload_news_to_db(json_list, search_topic):
             logging.error("Skipping item due to missing key: %s", e)
         except Exception as e:
             logging.error("Skipping item due to error: %s", e)
+
+    conn.close()
+
+def upload_fred_to_db(df):
+    """Check if the primary key exists in the database and upload data if not."""
+    logging.info("Starting upload to database.")
+    conn = sqlite3.connect(config.DATABASE)
+    c = conn.cursor()
+
+    for _, row in df.iterrows():
+        try:
+            date_value = row.get("Date", "")
+
+            # Check if the primary key (date, ticker) exists in the database
+            c.execute(
+                f"SELECT * FROM {config.TABLE_FRED_DATA} WHERE date=?",
+                (date_value),
+            )
+            result = c.fetchone()
+
+            if result:
+                logging.warning(
+                    "%s %s already in the database, skipping...", date_value
+                )
+            else:
+                # Insert new row into the database
+                c.execute(
+                    f"INSERT OR REPLACE INTO {config.TABLE_FRED_DATA} (date, exchange_rates, treasury_yields, fed_funds_rate, consumer_price_index, GDP, industrial_production, unemployment_rate, consumer_sentiment, producer_price_index) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    (
+                        date_value,
+                        row.get("exchange_rates", ""),
+                        row.get("treasury_yields", ""),
+                        row.get("fed_funds_rate", ""),
+                        row.get("consumer_price_index", ""),
+                        row.get("GDP", ""),
+                        row.get("industrial_production", ""),
+                        row.get("unemployment_rate", ""),
+                        row.get("consumer_sentiment", ""),
+                        row.get("producer_price_index", ""),
+                    ),
+                )
+                conn.commit()
+                logging.info(
+                    "UPLOADED: %s %s uploaded to the database", date_value
+                )
+        except Exception as e:
+            logging.error("Skipping row due to error: %s", e)
 
     conn.close()
--- config.py
+++ config.py
@@ -14,6 +14,7 @@ PROCESSED_DATA_PATH = Path(f"{CWD_PATH}/temp/data/processed").resolve()
 DATABASE = "news_wave_metrics.db"
 TABLE_STOCK_DATA = "stock_data"
 TABLE_NEWS_DATA = "ai_safety_news"
+TABLE_FRED_DATA = "fred"
 
 # === Stock Data Required Columns ===
 SELECTED_COLUMNS = ["Open", "High", "Low", "Close", "Volume"]
@@ -28,3 +29,5 @@ YEARS_OF_NEWS = 10
 
 #ECONOMIC_METRICS_IDS = [ Exchange Rates, Treasury Yields, Fed Funds Rate, CPI (Consumer Price Index), GDP (Gross Domestic Product), Industrial Production, Unemployment Rate, Consumer Sentiment, PPI (Producer Price Index)]
 ECONOMIC_METRICS_IDS = [ "DTWEXBGS", "T10Y2Y", "DFF", "CPIAUCSL", "GDP", "INDPRO", "UNRATE", "UMCSENT", "PPIACO"]
+
+OBSERVATION_START = '2014-01-01'
