971ba909398f778ef7352b72b01e3d0b2b35805b
Zohaib <zohaibshahid520@gmail.com>
2024-03-22 04:13:30 -0600

    Update load.py

--- load.py
+++ load.py
@@ -2,7 +2,7 @@ import config
 import logging
 import pprint
 from file_handler import FileHandler
-from SQLiteHandler import create_stock_table, upload_stock_to_db, create_news_table, upload_news_to_db
+from SQLiteHandler import create_stock_table, upload_stock_to_db, create_news_table, upload_news_to_db, create_fred_table, upload_fred_to_db
 
 pp = pprint.PrettyPrinter(indent=4)
 logging.basicConfig(
@@ -18,7 +18,6 @@ def load_stock_data(stock_data):
     This function loads the dataframe to the database.
     """
     logging.info("Loading Function Initiated")
-    print(stock_data)
     # Creating the db and table 
     create_stock_table()
     # Uploading the data to db
@@ -32,6 +31,14 @@ def load_news_data(search_topic):
     data = file_handler.load_json_files(directory=config.PROCESSED_DATA_PATH)
     create_news_table()
     upload_news_to_db(json_list=data, search_topic= search_topic)
+
+def load_fred_data(fred_data):
+    """
+    This function loads the JSON files from the processed folder and uploads them to the database.
+    """
+    logging.info("Loading Function Initiated")
+    create_fred_table()
+    upload_fred_to_db(df = fred_data)
 
 # if __name__ == "__main__":
 #     load_news_data("Federal Reserve")
