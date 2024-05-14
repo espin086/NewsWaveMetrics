d1bc610f9e97f9d3e0396472b0684e586c22bd78
JJ Espinoza <4843736+espin086@users.noreply.github.com>
2024-05-14 14:16:07 -0700

    updated with a new module called fetch_yfinance.py to be able to get information via the command line

---
+++ fetch_yfinance.py
@@ -0,0 +1,97 @@
+import argparse
+import yfinance as yf
+
+
+def fetch_info(ticker):
+    stock = yf.Ticker(ticker)
+    return stock.info
+
+
+def fetch_history(ticker, interval="1m"):
+    stock = yf.Ticker(ticker)
+    return stock.history(interval=interval)
+
+
+def fetch_actions(ticker):
+    stock = yf.Ticker(ticker)
+    return stock.actions
+
+
+def fetch_dividends(ticker):
+    stock = yf.Ticker(ticker)
+    return stock.dividends
+
+
+def fetch_splits(ticker):
+    stock = yf.Ticker(ticker)
+    return stock.splits
+
+
+def fetch_financials(ticker):
+    stock = yf.Ticker(ticker)
+    return {
+        "income_statement": stock.income_stmt,
+        "quarterly_income_statement": stock.quarterly_income_stmt,
+        "balance_sheet": stock.balance_sheet,
+        "quarterly_balance_sheet": stock.quarterly_balance_sheet,
+        "cash_flow": stock.cashflow,
+        "quarterly_cash_flow": stock.quarterly_cashflow,
+    }
+
+
+def fetch_holders(ticker):
+    stock = yf.Ticker(ticker)
+    return {
+        "major_holders": stock.major_holders,
+        "institutional_holders": stock.institutional_holders,
+        "mutualfund_holders": stock.mutualfund_holders,
+    }
+
+
+def fetch_recommendations(ticker):
+    stock = yf.Ticker(ticker)
+    return {
+        "recommendations_summary": stock.recommendations_summary,
+        "recommendations": stock.recommendations,
+    }
+
+
+def setup_argparse():
+    parser = argparse.ArgumentParser(description="Fetch stock data from Yahoo Finance.")
+    parser.add_argument("ticker", type=str, help="Stock ticker symbol.")
+    parser.add_argument(
+        "--data_type",
+        choices=[
+            "info",
+            "history",
+            "actions",
+            "dividends",
+            "splits",
+            "financials",
+            "holders",
+            "recommendations",
+        ],
+        help="Type of data to fetch.",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = setup_argparse()
+    functions = {
+        "info": fetch_info,
+        "history": fetch_history,
+        "actions": fetch_actions,
+        "dividends": fetch_dividends,
+        "splits": fetch_splits,
+        "financials": fetch_financials,
+        "holders": fetch_holders,
+        "recommendations": fetch_recommendations,
+    }
+    if args.data_type in functions:
+        result = functions[args.data_type](args.ticker)
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
